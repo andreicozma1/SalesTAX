de9707c4765c59b9c36ed1155af631b0990966f5
Andrei Cozma <andreig992@gmail.com>
2020-05-27 01:21:50 -0500

    Added first salestax example

--- SalesTax/ViewController.swift
+++ SalesTax/ViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var result: UILabel!
+    @IBOutlet var price: UITextField!
+    @IBOutlet var tax: UITextField!
+    
+  
+    
+    @IBAction func calculateTotal(_ sender: Any) {
+        let prc = Double(price.text!) ?? 0.0
+        let tx = Double(tax.text!) ?? 0.0
+            
+        let totalSalesTax = prc * (tx / 100.0)
+        let totalPrice = prc + totalSalesTax
+        
+        result.text = "$\(totalPrice)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+        
+        result.text = "$0.00"
         // Do any additional setup after loading the view.
     }
 
